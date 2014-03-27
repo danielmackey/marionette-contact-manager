ab16c5133c46a7be8dd2fd7b9d8a11f547071b9e
Daniel Mackey <daniel@danielmackey.com>
2014-03-26 22:48:32 -0700

    Refactored views to their own app/module

--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -12,7 +12,7 @@ ContactManager.module("Entities", function(Entities, ContactManager, Backbone, M
     contacts = new Entities.ContactCollection([
       { id: 1, firstName: 'Alice', lastName: 'Arten', phoneNumber: '555-0184' },
       { id: 2, firstName: 'Bob', lastName: 'Brigham', phoneNumber: '555-0163' },
-      { id: 3, firstName: 'Charlie', lastName: 'Campbell', phoneNumber: '555-0129' },
+      { id: 3, firstName: 'Charlie', lastName: 'Campbell', phoneNumber: '555-0129' }
     ])
   };
 
