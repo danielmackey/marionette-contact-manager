377edabd02db3edd3adcd635c53b763b5e86a424
Daniel Mackey <daniel@danielmackey.com>
2014-03-27 00:14:57 -0700

    Hook application up to local storage

--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -1,28 +1,44 @@
 ContactManager.module("Entities", function(Entities, ContactManager, Backbone, Marionette, $, _){
-  Entities.Contact = Backbone.Model.extend({});
+
+  Entities.Contact = Backbone.Model.extend({
+    urlRoot: 'contacts'
+  });
+
+  Entities.configureStorage(Entities.Contact);
 
   Entities.ContactCollection = Backbone.Collection.extend({
+    url: 'contacts',
     model: Entities.Contact,
     comparator: function(model) {
       return model.get('firstName') + model.get('lastName')
     }
   });
 
-  var contacts;
+
+  Entities.configureStorage(Entities.ContactCollection);
 
   var initializeContacts = function(){
-    contacts = new Entities.ContactCollection([
-      { id: 0, firstName: 'Alice', lastName: 'Tandem', phoneNumber: '555-0184' },
+    console.log("seed the db");
+    var contacts = new Entities.ContactCollection([
+      { id: 4, firstName: 'Alice', lastName: 'Tandem', phoneNumber: '555-2222' },
       { id: 1, firstName: 'Alice', lastName: 'Arten', phoneNumber: '555-0184' },
       { id: 2, firstName: 'Bob', lastName: 'Brigham', phoneNumber: '555-0163' },
       { id: 3, firstName: 'Charlie', lastName: 'Campbell', phoneNumber: '555-0129' }
     ])
+
+    contacts.forEach(function(contact){
+      contact.save();
+    });
+
+    return contacts;
   };
 
   var API = {
     getContactEntities: function(){
-      if(contacts === undefined){
-        initializeContacts()
+      var contacts = new Entities.ContactCollection()
+      contacts.fetch()
+      if(contacts.length === 0){
+        return initializeContacts();
       }
       return contacts;
     }
