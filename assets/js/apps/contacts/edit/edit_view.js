27b7b27fc4f19a42854e2636f3ba47343ef00591
Daniel Mackey <dmackey@apple.com>
2014-03-27 13:50:36 -0700

    Persist the edits, and redirect to show on save

--- assets/js/apps/contacts/edit/edit_view.js
+++ assets/js/apps/contacts/edit/edit_view.js
@@ -9,7 +9,8 @@ ContactManager.module("ContactsApp.Edit", function(Edit, ContactManager, Backbon
 
     submitClicked: function(e){
       e.preventDefault();
-      console.log("edit contact");
+      var data = Backbone.Syphon.serialize(this);
+      this.trigger("form:submit", data);
     }
   })
 
