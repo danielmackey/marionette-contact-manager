9f96a7a2d588a1312b9f550c3d45ce3300d76fd4
Daniel Mackey <dmackey@apple.com>
2014-03-27 15:26:43 -0700

    Update list and close modal after successful edit

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -29,6 +29,16 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
             asModal: true
           });
 
+          view.on("form:submit", function(data){
+            if(model.save(data)) {
+              childView.render();
+              ContactManager.dialogRegion.close();
+              childView.flash("success");
+            } else {
+              view.triggerMethod("form:data:invalid", model.validationError);
+            }
+          })
+
           ContactManager.dialogRegion.show(view);
         })
 
