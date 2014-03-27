05e308ff851b9a4a0a05f6af69c439d7485885a8
Daniel Mackey <daniel@danielmackey.com>
2014-03-26 23:58:46 -0700

    Refactor to use url routes for list and show

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -16,7 +16,7 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
       });
 
       contactsListView.on("itemview:contact:show", function(childView, model){
-        ContactManager.ContactsApp.Show.Controller.showContact(model);
+        ContactManager.trigger("contact:show", model.get('id'))
       });
 
       ContactManager.mainRegion.show(contactsListView);
