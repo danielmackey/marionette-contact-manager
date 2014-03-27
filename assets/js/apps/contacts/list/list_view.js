550877dfe9e392519155d92b65d52064b490d6b6
Daniel Mackey <daniel@danielmackey.com>
2014-03-26 23:14:40 -0700

    Adding delete button, and view logic to handle deleting contact

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,7 +1,21 @@
 ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _){
   List.Contact = Marionette.ItemView.extend({
     tagName: 'tr',
-    template: '#contact-list-item'
+    template: '#contact-list-item',
+
+    events: {
+      'click': 'highlightName',
+      'click button.js-delete': 'deleteClicked'
+    },
+
+    highlightName: function(e) {
+      this.$el.toggleClass('warning');
+    },
+
+    deleteClicked: function(e) {
+      e.stopPropagation();
+      this.model.collection.remove(this.model);
+    }
   });
 
   List.Contacts = Marionette.CompositeView.extend({
